02d0decf8b7b8b66434321755c8f55f25233dfa6
SakshiDevadiga <145028688+SakshiDevadiga@users.noreply.github.com>
2025-04-21 23:24:34 +0530

    Add files via upload

---
+++ register.js
@@ -0,0 +1,13 @@
+document.getElementById("registerForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+  
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+  
+    const user = { name, email, password };
+    localStorage.setItem("user", JSON.stringify(user));
+  
+    alert("Registration successful!");
+    window.location.href = "login.html";
+  });
